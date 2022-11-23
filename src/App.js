72d396d24eed9e56f03ae9be1369e1beb6fca8bb
sivin20 <sivin@student.sdu.dk>
2022-11-23 12:56:27 +0100

    Adds basic routing and a frame for the 4 pages needed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import FrontPage from './pages/FrontPage';
+import SearchPage from './pages/SearchPage';
+import RealtorProfilePage from './pages/RealtorProfilePage';
+import AppartmentPage from './pages/AppartmentPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route index element={<FrontPage/>}/>
+      <Route path="/search" element={<SearchPage/>}/>
+      <Route path="/appartment" element={<AppartmentPage/>}/>
+      <Route path="/realtor" element={<RealtorProfilePage/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/RealtorProfilePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function RealtorProfilePage() {
+    
+    return(
+        <>
+            <h1>Realtor profile page</h1>
+        </>
+    )
+}
