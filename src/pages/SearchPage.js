8485ac397055960550da65330921d390c68eab50
hannehue <hannehue@gmail.com>
2022-11-24 18:28:41 +0100

    Grid layout, init searchbar, sidebar and listings

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,12 +1,30 @@
-
 import React from 'react';
+import "../styles/search.css"
 
 export default function SearchPage() {
 
 
-    return(
+    return (
         <>
-            <h1>Search page</h1>
+            <section className='flex'>
+                <div className='search-bar'>
+                    <label className='search-label' htmlFor='searchBar'>Search</label>
+                    <input type="text" name='searchBar' id='searchBar' placeholder='Where do you want to live'></input>
+                </div>
+                <div className='grid content-wrapper'>
+                    <div className='sidebar'>
+                        <h1>lesgo</h1>
+                    </div>
+                    <div className='search-results'>
+                        <img src='https://image-lambda.boligportal.dk/3e5fdb5057d9f0a34ef60b6f08c4ff7f?auto=compress,enhance,format&h=800' className='image' />
+                        <img src='https://image-lambda.boligportal.dk/3e5fdb5057d9f0a34ef60b6f08c4ff7f?auto=compress,enhance,format&h=800' className='image' />
+                        <img src='https://image-lambda.boligportal.dk/3e5fdb5057d9f0a34ef60b6f08c4ff7f?auto=compress,enhance,format&h=800' className='image' />
+                        <img src='https://image-lambda.boligportal.dk/3e5fdb5057d9f0a34ef60b6f08c4ff7f?auto=compress,enhance,format&h=800' className='image' />
+                        <img src='https://image-lambda.boligportal.dk/3e5fdb5057d9f0a34ef60b6f08c4ff7f?auto=compress,enhance,format&h=800' className='image' />
+                        <img src='https://image-lambda.boligportal.dk/3e5fdb5057d9f0a34ef60b6f08c4ff7f?auto=compress,enhance,format&h=800' className='image' />
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
