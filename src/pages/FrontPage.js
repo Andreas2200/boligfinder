b32a5634078284a840f390b0f84042ef5f4a8cc4
VictorFrank <Victorbruun@github.com>
2022-11-24 15:28:23 +0100

    added search bar to frontpage

--- src/pages/FrontPage.js
+++ src/pages/FrontPage.js
@@ -1,10 +1,38 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export default function FrontPage() {
-    return(
-        <>
-            <h1>Frontpage</h1>
-        </>
+function SearchBar() {
+    const [input, setInput] = useState('');
+
+    //const inputRef = useRef(null);
+
+    /*useEffect(() => {
+        inputRef.current.focus()
+    });*/
+
+    const handleChange = e => {
+        setInput(e.target.value)
+    }
+
+    const handleSearch = e => {
+        e.preventDefault();
+        setInput('')
+    };
+
+    return (
+        <form className="" onSubmit={handleSearch}>
+            <input
+                type="text"
+                placeholder="Search real estate"
+                value={input}
+                name="text"
+                className="search-input"
+                onChange={handleChange}
+                //ref={inputRef}
+            />
+            <button Name="search-button"> Search</button>
+        </form>
     )
 }
+
+export default SearchBar
