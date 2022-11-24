3aa664676b3f967c3653528dbfa0fef720991136
hannehue <hannehue@gmail.com>
2022-11-24 18:44:25 +0100

    Started work on sidebar

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -13,7 +13,31 @@ export default function SearchPage() {
                 </div>
                 <div className='grid content-wrapper'>
                     <div className='sidebar'>
-                        <h1>lesgo</h1>
+                        <div className='flex filter-box'>
+                            <h1>Boligtype</h1>
+                            <div className='flex filter-checkbox'>
+                                <label htmlFor='lejligheder'>Lejligheder</label>
+                                <input type="checkbox" name="lejligheder" id="lejligheder"/>
+                            </div>
+                            <div className='flex filter-checkbox'>
+                                <label htmlFor='lejligheder'>Værelser</label>
+                                <input type="checkbox" name="værelser" id="værelser"/>
+                            </div>
+                            <div className='flex filter-checkbox'>
+                                <label htmlFor='lejligheder'>Huse</label>
+                                <input type="checkbox" name="huse" id="huse"/>
+                            </div>
+                            <div className='flex filter-checkbox'>
+                                <label htmlFor='lejligheder'>Rækkehuse</label>
+                                <input type="checkbox" name="rækkehuse" id="rækkehuse"/>
+                            </div>                            
+                        </div>
+                        <div className='flex filter-box'>
+                            <div className='rent-input'>
+                                <label htmlFor='rent'>Rent</label>
+                                <input type="number" name="rent" id="rent"/>
+                            </div>
+                        </div>
                     </div>
                     <div className='search-results'>
                         <img src='https://image-lambda.boligportal.dk/3e5fdb5057d9f0a34ef60b6f08c4ff7f?auto=compress,enhance,format&h=800' className='image' />
