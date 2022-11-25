ffd1cd2fe0dd5cf0fb2456ac99be61d2ac11c785
VictorFrank <Victorbruun@github.com>
2022-11-25 21:15:10 +0100

    Added css flies to for the frontpage, but things are still not loading properly when running the app

--- src/pages/FrontPage.js
+++ src/pages/FrontPage.js
@@ -1,5 +1,8 @@
 
 import React, { useEffect, useState } from 'react';
+import { ApartmentListing } from "../components/ApartmentListing";
+
+
 
 function FrontPage() {
     const [input, setInput] = useState('');
@@ -20,29 +23,44 @@ function FrontPage() {
     };
 
     return (
-        <section class="frontPage">
-            <div class="grid">
-                <div class="searchBar">
-                    <form className="" onSubmit={handleSearch}>
-                        <input
-                            type="text"
-                            placeholder="Where do you want to live?"
-                            value={input}
-                            name="text"
-                            className="search-input"
-                            onChange={handleChange}
-                        //ref={inputRef}
-                        />
-                        <button Name="search-button"> Search</button>
-                    </form>
+        <>
+            <section class="frontPage">
+                <div class="grid">
+                    <div class="title">
+                        <h1>BoligFinder</h1>
+                    </div>
+                    <div class="searchBar">
+                        <form className="" onSubmit={handleSearch}>
+                            <input
+                                type="text"
+                                placeholder="Where do you want to live?"
+                                value={input}
+                                name="text"
+                                className="search-input"
+                                onChange={handleChange}
+                            //ref={inputRef}
+                            />
+                            <button Name="search-button"> Search</button>
+                        </form>
+                    </div>
+                    <div className="shortcuts">
+                        <div className="shortcut1">
+                            <h1>Odense shortcut</h1>
+                        </div>
+                        <div className="shortcut2">
+                            <h1>Aarhus shortcut</h1>
+                        </div>
+                        <div className="shortcut3">
+                            <h1>København shortcut</h1>
+                        </div>
+                    </div>
+                    <div class="listings">
+                        <ApartmentListing title={"2 værelses lejlighed"} imagepath={"resources/dormroom.JPG"} city={"Odense M"} />
+                        <ApartmentListing title={"4 værelses lejlighed"} imagepath={"resources/studioapartment.JPeG"} city={"Ringkøbing"} />
+                    </div>
                 </div>
-                <div className="shortcuts">
-
-                </div>
-                <div class="listings">
-                </div>
-            </div>
-        </section>
+            </section>
+        </>
     )
 }
 
